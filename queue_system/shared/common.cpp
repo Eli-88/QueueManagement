0d6eb328e4f49c1ee664f779e76637d269e778f9
Eli-88 <elilim88@gmail.com>
2021-11-26 12:39:36 +0800

    fix linux compliation issue for thread set name

--- queue_system/shared/common.cpp
+++ queue_system/shared/common.cpp
@@ -5,9 +5,14 @@ namespace queue_system {
 DetachableThread::DetachableThread(const char *name,
                                    const std::function<void()> &impl) {
   thread_ = std::move(std::thread([&] {
+#ifdef __APPLE__
     pthread_setname_np(name);
+#endif
     impl();
   }));
+#ifdef __linux__
+  pthread_setname_np(thread_.native_handle(), name);
+#endif
   thread_.detach();
 }
 } // namespace queue_system
